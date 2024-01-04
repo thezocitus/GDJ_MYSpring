7439174ff5fb529fee340c933063028c7fa06267
Zominwoo <thezoticus@gmail.com>
2024-01-04 12:10:36 +0900

    1
    1

--- src/main/java/com/winter/app/HomeController.java
+++ src/main/java/com/winter/app/HomeController.java
@@ -6,13 +6,13 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.winter.app.ioc.Robot;
+
 
 /**
  * Handles requests for the application home page.
@@ -25,10 +25,7 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-//	@Autowired
-//	private Robot robot;
-		
-	//annotation : 설명 + 기능
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
@@ -40,9 +37,7 @@ public class HomeController {
 		
 		model.addAttribute("serverTime", formattedDate );
 		
-//		robot.getLeftArm().act();
-//		robot.getRightArm().act();
-		
+
 		
 		return "index";
 	}
